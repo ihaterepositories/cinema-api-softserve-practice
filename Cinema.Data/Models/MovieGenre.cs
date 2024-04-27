57eead5dd30d072aeb59953f9a81eec254d9e211
VladPaliychuk <ssmxtxl@gmail.com>
2024-04-27 23:05:12 +0300

    add configurations

--- Cinema.Data/Models/MovieGenre.cs
+++ Cinema.Data/Models/MovieGenre.cs
@@ -5,6 +5,6 @@ public class MovieGenre
     public Guid MovieId { get; set; }
     public Guid GenreId { get; set; }
     
-    public virtual Movie Movie { get; set; } = null!;
-    public virtual Genre Genre { get; set; } = null!;
+    public Movie Movie { get; set; } = null!;
+    public Genre Genre { get; set; } = null!;
 }
