591d1f6ad388c6cc74a85261763359dbea63caf1
VladPaliychuk <ssmxtxl@gmail.com>
2024-04-27 23:05:25 +0300

    add configurations

---
+++ Cinema.DAL/Configurations/ActorConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Cinema.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.DAL.Configurations;
+
+public class ActorConfiguration : IEntityTypeConfiguration<Actor>
+{
+    public void Configure(EntityTypeBuilder<Actor> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.FullName)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(a => a.Description)
+            .HasMaxLength(500);
+
+        builder.Property(a => a.Photo)
+            .HasMaxLength(200);
+
+        builder.HasMany(a => a.MovieActors)
+            .WithOne()
+            .HasForeignKey(ma => ma.ActorId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Cinema.DAL/Configurations/ReservationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Cinema.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.DAL.Configurations;
+
+public class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
+{
+    public void Configure(EntityTypeBuilder<Reservation> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.HasMany(r => r.ReservedSeats)
+            .WithOne()
+            .HasForeignKey(rs => rs.ReservationId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
