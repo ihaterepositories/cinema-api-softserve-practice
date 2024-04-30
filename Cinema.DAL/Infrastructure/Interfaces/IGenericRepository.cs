f8591fe34e62ec6811c9f04b6149509ad4af8010
Bohutskiy Oleh <bogucukij@gmail.com>
2024-04-30 13:11:32 +0300

    generic repository added

---
+++ Cinema.DAL/Infrastructure/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema.DAL.Infrastructure.Interfaces;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    Task<List<TEntity>> GetAsync();
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task InsertAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(Guid id);
+}
