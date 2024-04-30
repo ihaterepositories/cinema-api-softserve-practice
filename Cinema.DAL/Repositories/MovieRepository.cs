a3fd469dab4a4e8282991bea89fb6c2426634c35
VladPaliychuk <ssmxtxl@gmail.com>
2024-04-30 13:44:36 +0300

    add repositories with interfaces

---
+++ Cinema.DAL/Repositories/MovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.DAL.Infrastructure;
+using Cinema.DAL.Repositories.Interfaces;
+using Cinema.Data.Models;
+
+namespace Cinema.DAL.Repositories;
+
+public class MovieRepository : GenericRepository<Movie>, IMovieRepository
+{
+    public MovieRepository(CinemaContext context) : base(context)
+    {
+    }
+}
---
+++ Cinema.DAL/Repositories/ScreeningRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.DAL.Infrastructure;
+using Cinema.DAL.Repositories.Interfaces;
+using Cinema.Data.Models;
+
+namespace Cinema.DAL.Repositories;
+
+public class ScreeningRepository : GenericRepository<Screening>, IScreeningRepository
+{
+    public ScreeningRepository(CinemaContext context) : base(context)
+    {
+    }
+}
