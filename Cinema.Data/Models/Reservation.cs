05647acc6a65c97bd9b6f9ae2acad16f886268c2
Danyel Khodoba <khodoba.danyel@chnu.edu.ua>
2024-05-11 13:49:11 +0300

    changed-reservations-and-seats-reserved

--- Cinema.Data/Models/Reservation.cs
+++ Cinema.Data/Models/Reservation.cs
@@ -3,10 +3,8 @@
 public class Reservation
 {
     public Guid Id { get; set; }
-    public bool Reserved { get; set; }
     public Guid UserId { get; set; }
     public bool IsPaid { get; set; }
-    public bool IsActive { get; set; }
 
     public ICollection<ReservedSeat> ReservedSeats { get; set; } = null!;
     public User User { get; set; } = null!;
