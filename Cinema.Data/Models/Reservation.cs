b3aa648dc92cd1cc3bba55d1f66da628735fbd2e
Bohutskiy Oleh <bogucukij@gmail.com>
2024-05-17 15:41:33 +0300

    Validation for entity`s fields added

--- Cinema.Data/Models/Reservation.cs
+++ Cinema.Data/Models/Reservation.cs
@@ -1,9 +1,15 @@
-﻿namespace Cinema.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Data.Models;
 
 public class Reservation
 {
     public Guid Id { get; set; }
+    
+    [Required]
     public Guid UserId { get; set; }
+    
+    [Required]
     public bool IsPaid { get; set; }
 
     public ICollection<ReservedSeat> ReservedSeats { get; set; } = null!;
--- Cinema.Data/Models/Room.cs
+++ Cinema.Data/Models/Room.cs
@@ -1,9 +1,16 @@
-﻿namespace Cinema.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Data.Models;
 
 public class Room
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [StringLength(100)]
     public string RoomName { get; set; } = string.Empty;
+    
+    [Range(1, 50)]
     public int NumberOfSeats { get; set; }
     
     public ICollection<Screening> Screenings { get; set; } = null!;
