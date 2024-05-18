bd2915ebb57ede3e2a8c4df79db419436b899862
VladPaliychuk <ssmxtxl@gmail.com>
2024-05-19 02:25:14 +0300

    add authorize check

--- Cinema.API/Controllers/RecommndationController.cs
+++ Cinema.API/Controllers/RecommndationController.cs
@@ -1,11 +1,13 @@
 ﻿using Cinema.BLL.Services;
 using Cinema.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize(Policy = "OnlyUser")]
 public class RecommendationsController : ControllerBase
 {
     private readonly IRecommendationService _recommendationService;
