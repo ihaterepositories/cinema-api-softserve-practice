de33940f5d892960495c937836b7167b7cf63a92
Danyel Khodoba <khodoba.danyel@chnu.edu.ua>
2024-05-19 23:58:15 +0300

    Revert "Test"
    This reverts commit 4ab24f9167f89787c9a46e7cbf47163377cfceef.

--- Cinema.DAL/obj/Debug/net8.0/Cinema.DAL.AssemblyInfo.cs
+++ Cinema.DAL/obj/Debug/net8.0/Cinema.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinema.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea7e4e58e6f1cece0988e4e028029d3bda6cb89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7acedb1c05e33e0f79809fb5ef1c016424dfb564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinema.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinema.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
