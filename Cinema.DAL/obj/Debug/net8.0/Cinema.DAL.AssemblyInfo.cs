2ea7e4e58e6f1cece0988e4e028029d3bda6cb89
SteveKSV <amigo.kl08@gmail.com>
2024-05-19 23:15:14 +0300

    Dev into main

--- Cinema.DAL/obj/Debug/net8.0/Cinema.DAL.AssemblyInfo.cs
+++ Cinema.DAL/obj/Debug/net8.0/Cinema.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinema.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a024b7dfa24e3141162065bdc96d83a12547f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7acedb1c05e33e0f79809fb5ef1c016424dfb564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinema.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinema.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
